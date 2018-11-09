310699baf148298b7f71e7bafa9d0a7063712370
Victor Sun <vsun@osausa.com>
2018-11-09 11:30:31 -0500

    V

--- authorquiz/src/AuthorQuiz.js
+++ authorquiz/src/AuthorQuiz.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
+
+function Hero(){
+  return (<div className="row"> 
+	<div className="jumbotron col-10 offset-1">
+		<h1>Author Quiz</h1>
+		<p>Select the book written by the author shown</p>
+	</div>t
+  </div>)
+}
+
+function Turn(){
+	return (<div/>)
+}
+
+function Continue(){
+	return (<div/>)
+}
+
+function Footer(){
+	return (<div id="footer" className="row">
+		<div className = "col-12">
+			<p  className="text-muted credit">
+				All images are from
+			</p>
+		</div>
+	</div>)
+}
 
 class AuthorQuiz extends Component {
   render() {
     return (
-      <div>AuthorQuiz</div>
-    );
+      <div className="container-fluid">
+        <Hero/> 
+        <Turn/>
+        <Continue/>
+		<Footer>
+      </div>
+    ); 
   }
 }
 
